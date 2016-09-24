a7b2e89ea0be1797a579e91b8c34a750688086d4
Dieter Laus <dieter.laus@isa-laus.de>
2016-09-24 11:08:05 +0200

    Implementierung

---
+++ isa.Math.Test/UnitRussianMultiplication.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace isa.Math.Test
+{
+    /// <summary>Tests für Russische Bauernmultiplikation</summary>
+    /// <remarks> isa.Math.dll {  isa.Math.RussianMultiplication.Mul } </remarks>
+
+    [TestClass]
+    public class UnitRussianMultiplication
+    {
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(Random.Next, Random.Next)</summary>
+        /// <remarks> Zahlenreihe von Multiplikatoren,Multiplikant (20 mal Random 0-1000) </remarks>
+        [TestMethod]
+        public void RussianMultiplicationRandomRangeValue()
+        {
+            Random rnd = new Random();
+            for (int ctr = 1; ctr <= 20; ctr++)
+            {
+                int multiplier = rnd.Next(0, 1000);
+                int multiplicand = rnd.Next(0, 1000);
+                int product = isa.Math.RussianMultiplication.Mul(multiplicand, multiplier);
+                Assert.AreEqual(product, multiplicand * multiplier);
+            }
+
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(100, 0)</summary>
+        /// <remarks>Multiplikator ist 0  (multiplier == 0)</remarks>
+        [TestMethod]
+        public void RussianMultiplicationMultiplierZero()
+        {
+            int product = isa.Math.RussianMultiplication.Mul(100, 0);
+            Assert.AreEqual(product, 100 * 0);
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(+, -)</summary>
+        /// <remarks>Multiplikator ist eine negative zahl (multiplier kleiner 0)</remarks>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void RussianMultiplicationMultiplierNegation()
+        {
+            isa.Math.RussianMultiplication.Mul(5, -100);
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(0, 100)</summary>
+        /// <remarks>Multiplikant ist 0 (multiplicand == 0)</remarks>
+        [TestMethod]
+        public void RussianMultiplicationMultiplicandZero()
+        {
+            int product = isa.Math.RussianMultiplication.Mul(0, 100);
+            Assert.AreEqual(product, 0 * 100);
+
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(-, +)</summary>
+        /// <remarks>Multiplikant ist eine negative Zahl / ExpectedException ArgumentOutOfRangeException</remarks>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void RussianMultiplicationMultiplicandNegation()
+        {
+            isa.Math.RussianMultiplication.Mul(-2, 100);
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(0, 0)</summary>
+        /// <remarks>Multiplikant und  Multiplikator sind 0 (multiplicand == 0) (multiplier == 0)</remarks>
+        [TestMethod]
+        public void RussianMultiplicationMultiplicandAndMultiplierZero()
+        {
+            int product = isa.Math.RussianMultiplication.Mul(0, 0);
+            Assert.AreEqual(product, 0 * 0);
+        }
+
+        /// <summary>Test isa.Math.RussianMultiplication.Mul(-, -)</summary>
+        /// <remarks>Multiplikant und  Multiplikator sind eine negative Zahl / (multiplier kleiner 0) (multiplicand kleiner 0)  / ExpectedException ArgumentOutOfRangeException </remarks>
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void RussianMultiplicationMultiplicandAndMultiplierNegation()
+        {
+            isa.Math.RussianMultiplication.Mul(-10, -100);
+        }
+    }
+
+}
---
+++ isa.Math/RussianMultiplication.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isa.Math
+{
+    /// <summary>Russische Bauernmultiplikation</summary>
+    /// <remarks>Die Russische Bauernmultiplikation (auch Ägyptisches Multiplizieren, Abessinische Bauernregel oder Verdopplungs-Halbierungs-Methode genannt) 
+    /// ist ein einfaches Verfahren zur Multiplikation zweier natürlicher Zahlen.</remarks>
+    public static class RussianMultiplication
+    {
+        /// <summary>Mul bildet das einfache Verfahren zur Multiplikation zweier natürlicher Zahlen ab. </summary>
+        /// <param name="x">int , Multiplikand</param>
+        /// <param name="y"> int , Multiplikator</param>
+        /// <returns> int , Das Produkt der Multiplikation</returns>
+        /// <remarks>https://de.wikipedia.org/wiki/Russische_Bauernmultiplikation</remarks>
+        /// <exception cref="System.ArgumentOutOfRangeException"> Bei Funktionsaufruf(multiplicand,multiplier) mit nicht natürlichen Zahlen</exception>
+        /// <example>
+        /// Dieses Beispiel zeigt die Verwendung der Mathematischen-Extension <see cref="Left"/>.
+        /// <code>
+        ///     isa.Math.RussianMultiplication.Mul(47,42);
+        ///     //Returns: 1974
+        /// </code>
+        /// </example>
+        /// 
+        public static int Mul(int x, int y)
+        {
+            int multiplicand = x;
+            int multiplier = y;
+            /*Zielzustand*/
+            if (multiplier == 1) { return multiplicand; }
+
+            /* Prüfung auf natürliche Zahlen (Null mit einbezogen) */
+            if (multiplier < 0) { throw new ArgumentOutOfRangeException("y", "Nur natürliche Zahlen sind gültig."); }
+            if (multiplicand < 0) { throw new ArgumentOutOfRangeException("x", "Nur natürliche Zahlen sind gültig."); }
+
+            /* Ausnahme für 0 */
+            if (multiplier == 0) { return 0; }
+            if (multiplicand == 0) { return 0; }
+
+            /* Durch die Teilung  multiplier/2 wird automatisch auf eine natürliche Zahl abgerundet (abschneiden der Nachkommastellen) */
+            if (multiplier % 2 == 1)
+            {
+                return multiplicand + isa.Math.RussianMultiplication.Mul(2 * multiplicand, multiplier / 2);
+            }
+            else
+            {
+                return isa.Math.RussianMultiplication.Mul(2 * multiplicand, multiplier / 2);
+            }
+        }
+    }
+
+}
